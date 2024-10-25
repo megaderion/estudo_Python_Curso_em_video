df640db0a28eec66127be8e54065addf5fad8dfa
Matheus Francisco <112489067+megaderion@users.noreply.github.com>
2024-10-25 09:07:50 -0300

    Estudo de variaveis
    Estudando Variáveis, brincando com input

---
+++ estudo.py
@@ -0,0 +1,20 @@
+#Variaveis 
+#input 
+
+ #nome =  input("Qual é o seu nome?")
+ #idade = 29
+ #peso = 84
+
+ # print("olá", nome, "prazer te conhecer!")
+
+#dia= input("dia = ")
+#mes= input("mes = ")
+#ano= input("ano = ")
+
+#print ("você nasceu no dia ", dia, " de", mes, "de", ano," .Correto?" )
+
+#num1= int(input("primeiro número "))
+#num2= int(input("segundo numero "))
+#soma= num1 + num2
+
+#print("a soma é", soma)
