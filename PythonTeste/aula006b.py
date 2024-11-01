a1f8becea3b33325aa1fcb393bfe5f7b857b4804
Matheus Francisco <112489067+megaderion@users.noreply.github.com>
2024-11-01 06:54:22 -0300

    Dissecando uma variável

--- PythonTeste/aula006b.py
+++ PythonTeste/aula006b.py
@@ -1,4 +1,11 @@
 #Faça um programa que leia algo pelo teclado e mostre nela o seu tipo primitivo  e todas as informações possiveis sobre ele.
 
 n = input("Digite algo: ")
-print(n.isnumeric)
+print("O tipo primitivo desse valor é", type(n))
+print("so tem espaços? ", n.isspace())
+print("é um numero?", n.isnumeric())
+print("é um alfabeto? ", n.isalpha())
+print("é um alfanumerico? ", n.isalnum())
+print("está em maiuscula? ", n.isupper())
+print("Está em minuscula? ", n.islower())
+print("Está capitalizada?", n.istitle())
