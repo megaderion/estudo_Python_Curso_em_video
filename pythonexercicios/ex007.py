a0e6c052b1106f116538f3e3e1d8da1b9353970c
Matheus Francisco <112489067+megaderion@users.noreply.github.com>
2024-11-12 07:49:51 -0300

    so melhoria visual

--- pythonexercicios/ex007.py
+++ pythonexercicios/ex007.py
@@ -1,6 +1,7 @@
 #Faça um programa que leia um número qualquer e mostre na tela sua tabuada
 
 n = int(input("Digite um número "))
+print("--"*9)
 print("{} x {:2} = {}".format(n, 1, n*1))
 print("{} x {:2} = {}".format(n, 2, n*2))
 print("{} x {:2} = {}".format(n, 3, n*3))
@@ -11,3 +12,4 @@ print("{} x {:2} = {}".format(n, 7, n*7))
 print("{} x {:2} = {}".format(n, 8, n*8))
 print("{} x {:2} = {}".format(n, 9, n*9))
 print("{} x {:2} = {}".format(n, 10, n*10))
+print("--"*9)
