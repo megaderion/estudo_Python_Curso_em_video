575abf40afadd688c8fcd5fb996f9066c61438ac
Matheus Francisco <112489067+megaderion@users.noreply.github.com>
2024-11-12 07:13:29 -0300

    corrigindo o exercicio
    Estava certo, so melhorando o código

--- pythonexercicios/ex003.py
+++ pythonexercicios/ex003.py
@@ -1,6 +1,4 @@
 #Faça um programa que leia um número inteiro e mostre na tela seu sucessor e seu antecessor
 
 n = int(input("Digite um número "))
-a = n-1
-s = n+1
-print("o antecessor é {}, e o sucessor desse número é {}".format(a, s))
+print("Analisando o número {} seu antecessor é {}, e o sucessor desse número é {}".format(n, (n-1), (n+1)))
