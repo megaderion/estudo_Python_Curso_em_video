0dbe6b91a67b536bd46f435e62d590e94aed429d
Matheus Francisco <112489067+megaderion@users.noreply.github.com>
2024-11-13 06:32:44 -0300

    corrigindo exercicios
    melhorando o codigo

--- pythonexercicios/ex011.py
+++ pythonexercicios/ex011.py
@@ -1,6 +1,5 @@
 # Faça um algoritmo que leia o salario de um funcionário e mostre seu novo salário com 15% de aumento.
 
 s = float(input("Qual o seu salário? "))
-a = (15 / 100) * s
-au = a + s
-print("Seu salário de {}R$ com 15% de aumento vai ficar {}R$".format(s, au))
+a = (s * 15 / 100) + s
+print("Seu salário de {:.2f}R$ com 15% de aumento vai ficar {:.2f}R$".format(s, a))
