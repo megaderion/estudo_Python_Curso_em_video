6a79adc24e13a5b9a6a87d23c40d4237f5147403
Matheus Francisco <112489067+megaderion@users.noreply.github.com>
2025-03-11 06:43:49 -0300

    Manipulando trexto
    Aprendendo operações com String em python

---
+++ PythonTeste/aula009.py
@@ -0,0 +1,6 @@
+#As principais operações que vamos aprender são o Fatiamento de String, Análise com len(), count(), find(), transformações com replace(), upper(), lower(), capitalize(), title(), strip(), junção com join().
+
+frase = "Curso em Video Python"
+#print(frase.replace("Python", "Flames"))
+#print(len(frase))
+#print(frase.split())
