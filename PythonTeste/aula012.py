687a23bacc7d08c2efb7d4ba3c2339943cbf9d9f
Matheus Francisco <112489067+megaderion@users.noreply.github.com>
2025-04-08 06:52:05 -0300

    condições aninhadas
    se aprofundando um pouco mais nas estruturas de condições, utilizando if, elif e else

---
+++ PythonTeste/aula012.py
@@ -0,0 +1,8 @@
+nome = str(input("Qual o seu nome? "))
+if nome == "Matheus":
+    print("Que nome bonito")
+elif nome == "Bernardo" or nome == "Jessica" or nome =="Theo":
+    print("Seu nome e dahora também")
+else:
+    print("Seu nome e bem normal")
+print("Tenha um otimo dia {}".format(nome))
